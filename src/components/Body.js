10a7b8b76f9ccc695083cbd49db57c2b74923037
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-16 09:46:05 +0530

    add: shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,7 +16,6 @@ const Body = () => {
 
     const json = await data.json();
 
-    console.log(json);
     const restaurants =
       json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
 
