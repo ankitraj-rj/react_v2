510172e018383af0ec5660d9ad7171edb8078430
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-22 10:22:06 +0530

    add: RestaurantMenu file and Setup

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,13 @@
+const RestaurantMenu = () => {
+  <div className="menu">
+    <h1>Name of the restaurant</h1>
+    <h2>Menu of the restaurant</h2>
+    <ul>
+      <li>Biryani</li>
+      <li>Burgers</li>
+      <li>Diet Coke</li>
+    </ul>
+  </div>;
+};
+
+export default RestaurantMenu;
