7603dca7e181b60e1c6a19b938b8e6bc9dfe94a9
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-23 10:07:03 +0530

    add: User.js setup

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 const RestaurantMenu = () => {
+  const [resInfo, setresInfo] = useState(null);
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6426028&lng=77.21921669999999&restaurantId=1257078&catalog_qa=undefined&submitAction=ENTER",
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+  };
+
+  if (resInfo === null) return <Shimmer />;
+
   return (
     <div className="menu">
       <h1>Name of the restaurant</h1>
