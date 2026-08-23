8776bca433b87642950c6e36cdc16f7cb325fc9e
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-23 10:53:16 +0530

    add: destructuring in class based component

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <h1>About Section</h1>
       <h3>This is Namaste React</h3>
       <User name={"Akshay Saini (function) "} />
-      <UserClass name={"Akshay Saini (class) "} />
+      <UserClass name={"Akshay Saini (class) "} location={"patna"} />
     </div>
   );
 };
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,10 +6,12 @@ class UserClass extends React.Component {
   }
 
   render() {
+    const { name, location } = this.props;
+
     return (
       <div className="user-card">
-        <h2>Name: {this.props.name}</h2>
-        <h3>Location: Bihar</h3>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
         <h4>Contact: akshaymarch7</h4>
       </div>
     );
