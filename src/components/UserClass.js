ef5797366fce2b51d3e3aaa7b332ccc58b59f2ae
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-23 12:20:13 +0530

    add: destructuring of state varibale in class based component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -11,10 +11,13 @@ class UserClass extends React.Component {
 
   render() {
     const { name, location } = this.props;
+    // destructuring state varibale ..
+    const { count } = this.state;
 
     return (
       <div className="user-card">
-        <h1>Count: {this.state.count}</h1>
+        {/* <h1>Count: {this.state.count}</h1> */}
+        <h1>Count: {count}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: akshaymarch7</h4>
