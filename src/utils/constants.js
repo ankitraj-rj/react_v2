7ba54c10f69821ff20acb33176e83506333868d1
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-26 22:08:43 +0530

    add: improvement in Constant.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/";
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/";
 
+export const SWIGGY_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6426028&lng=77.21921669999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const SWIGGY_MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6426028&lng=77.21921669999999&restaurantId=";
+
+export const getProxyUrl = (url) =>
+  `https://corsproxy.io/?url=${encodeURIComponent(url)}`;
