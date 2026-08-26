b4f0a2ec0c17653fa68c3f83c875bdfed31e4853
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-26 22:05:44 +0530

    add: improvement in User.js

--- src/components/User.js
+++ src/components/User.js
@@ -6,9 +6,18 @@ const User = ({ name, location }) => {
   return (
     <div className="user-card">
       <h1>Count: {count}</h1>
-      <h2>Name: {name}</h2>
-      <h3>Location: {location}</h3>
-      <h4>Contact: akshaymarch7</h4>
+
+      <h2>
+        Name: {name}
+      </h2>
+
+      <h3>
+        Location: {location}
+      </h3>
+
+      <h4>
+        Contact: akshaymarch7
+      </h4>
     </div>
   );
 };
