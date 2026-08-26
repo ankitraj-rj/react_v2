b981f911bf9c0e944bdebf16c72810887dc486db
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-26 22:04:43 +0530

    add: improvement in Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,16 +1,14 @@
 const Shimmer = () => {
   return (
     <div className="shimmer-container">
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
+      {Array.from({ length: 10 }).map(
+        (_, index) => (
+          <div
+            className="shimmer-card"
+            key={index}
+          ></div>
+        )
+      )}
     </div>
   );
 };
