158fb824b13b28c7cf1dca3c527c9f24e8f8a053
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-26 22:03:19 +0530

    add: improvement in RestaurantCard.js

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,33 +1,52 @@
+import { Link } from "react-router-dom";
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = ({ resData }) => {
   const {
+    id,
     name,
     cloudinaryImageId,
-    cuisines,
+    cuisines = [],
     avgRating,
     costForTwo,
-    sla: { deliveryTime },
+    sla,
   } = resData.info;
 
+  const deliveryTime = sla?.deliveryTime;
+
   return (
-    <div className="res-card">
-      <img
-        className="res-logo"
-        src={CDN_URL + cloudinaryImageId}
-        alt={name}
-      />
+    <Link
+      to={`/restaurants/${id}`}
+      className="res-link"
+    >
+      <div className="res-card">
+        <img
+          className="res-logo"
+          src={CDN_URL + cloudinaryImageId}
+          alt={name}
+        />
 
-      <h3>{name}</h3>
+        <h3>{name}</h3>
 
-      <h4>{cuisines.join(", ")}</h4>
+        <h4>
+          {cuisines.join(", ")}
+        </h4>
 
-      <h4>⭐ {avgRating}</h4>
+        <h4>
+          ⭐ {avgRating}
+        </h4>
 
-      <h4>{deliveryTime} mins</h4>
+        <h4>
+          {deliveryTime
+            ? `${deliveryTime} mins`
+            : "Delivery time unavailable"}
+        </h4>
 
-      <h4>{costForTwo}</h4>
-    </div>
+        <h4>
+          {costForTwo || "Price unavailable"}
+        </h4>
+      </div>
+    </Link>
   );
 };
 
