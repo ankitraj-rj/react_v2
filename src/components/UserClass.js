35586ab00a2c8b248a1b1347bfb4b0e5a4ed570b
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-26 22:06:10 +0530

    add: improvement in UserClass.js

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -10,34 +10,60 @@ class UserClass extends React.Component {
   }
 
   async componentDidMount() {
-    const data = await fetch("https://randomuser.me/api/");
-    const jsonData = await data.json();
+    try {
+      const response = await fetch(
+        "https://randomuser.me/api/"
+      );
 
-    this.setState({
-      userInfo: jsonData.results[0],
-    });
+      const jsonData =
+        await response.json();
+
+      this.setState({
+        userInfo: jsonData.results[0],
+      });
+    } catch (error) {
+      console.error(
+        "User API Error:",
+        error
+      );
+    }
   }
 
   render() {
     if (this.state.userInfo === null) {
-      return <h2>Loading...</h2>;
+      return (
+        <h2>Loading...</h2>
+      );
     }
 
-    const { name, location, gender, picture } = this.state.userInfo;
+    const {
+      name,
+      location,
+      gender,
+      picture,
+    } = this.state.userInfo;
 
     return (
       <div className="user-card">
-        <img src={picture.large} alt={`${name.first} ${name.last}`} />
+        <img
+          src={picture.large}
+          alt={`${name.first} ${name.last}`}
+        />
 
         <h2>
-          Name: {name.first} {name.last}
+          Name: {name.first}{" "}
+          {name.last}
         </h2>
 
         <h3>
-          Location: {location.city}, {location.country}
+          Location:{" "}
+          {location.city},{" "}
+          {location.country}
         </h3>
 
-        <h4>Gender: {gender}</h4>
+        <h4>
+          Gender: {gender}
+        </h4>
       </div>
     );
   }
