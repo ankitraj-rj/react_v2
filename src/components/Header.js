2f454b594332c08d369f925219b7b21f4d578978
Ankit Raj <contactankitrajofficial@gmail.com>
2026-08-26 22:02:04 +0530

    add: improvement in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,46 +2,55 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [btnNameReact, setbtnNameReact] = useState("Login");
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  const handleLogin = () => {
+    setBtnNameReact((prev) =>
+      prev === "Login" ? "Logout" : "Login"
+    );
+  };
 
   return (
-    <div className="header">
+    <header className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src="https://template.canva.com/EAGRMiB2YSc/1/0/800w-_QP2AMKVQJM.jpg"
-          alt="logo"
-        />
+        <Link to="/">
+          <img
+            className="logo"
+            src="https://template.canva.com/EAGRMiB2YSc/1/0/800w-_QP2AMKVQJM.jpg"
+            alt="Food App Logo"
+          />
+        </Link>
       </div>
 
-      <div>
+      <nav>
         <ul className="nav-list">
           <li>
             <Link to="/">Home</Link>
           </li>
+
           <li>
-            <Link to="/about"> About Us</Link>
+            <Link to="/about">About Us</Link>
           </li>
+
           <li>
             <Link to="/contact">Contact Us</Link>
           </li>
+
           <li>
             <Link to="/cart">Cart</Link>
           </li>
 
-          <button
-            className="login"
-            onClick={() => {
-              btnNameReact === "Login"
-                ? setbtnNameReact("Logout")
-                : setbtnNameReact("Login");
-            }}
-          >
-            {btnNameReact}
-          </button>
+          <li>
+            <button
+              className="login"
+              onClick={handleLogin}
+            >
+              {btnNameReact}
+            </button>
+          </li>
         </ul>
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 };
 
